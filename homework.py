ca7b1869d1c30fb22c1fee5d5c32e6de2f405f2d
VeraFaust <110284629+VeraFaust@users.noreply.github.com>
2023-11-29 15:25:53 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -44,30 +44,30 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    """Доступность переменных окружения,."""
-    """необходимых для работы программы."""
+    """Доступность переменных окружения,
+    необходимых для работы программы."""
     tokens = [
         PRACTICUM_TOKEN,
         TELEGRAM_TOKEN,
         TELEGRAM_CHAT_ID
     ]
     if not all(tokens):
-        message = 'Переменная окружения остуствует или не задана'
+        message = 'Переменная окружения остуствует или не задана!'
         logger.critical(message)
         return False
     return True
 
 
 def send_message(bot, message):
-    """Отправление сообщения в Telegram чат."""
+    """Отправка сообщения в Telegram чат."""
     try:
         bot.send_message(
             TELEGRAM_CHAT_ID,
             message
         )
-        logger.debug('Сообщение отправлено')
+        logger.debug('Сообщение отправлено.')
     except Exception as error:
-        logger.error(f'Сообщение не отправлено. {error}')
+        logger.error(f'Сообщение не отправлено. {error}!')
 
 
 def get_api_answer(timestamp):
@@ -82,49 +82,49 @@ def get_api_answer(timestamp):
         response = requests.get(REQUEST)
     except Exception as error:
         raise ApiResponseError(
-            f'Ошибка при запросе к основному API: {error}'
+            f'Ошибка при запросе к основному API: {error}!'
         )
     else:
-        logger.info('Ответ от API получен')
+        logger.info('Ответ от API получен.')
     if response.status_code != HTTPStatus.OK:
         raise ApiResponseError(
-            'Ошибка при запросе к основному API'
+            'Ошибка при запросе к основному API.'
         )
     try:
         response = response.json()
     except json.decoder.JSONDecodeError:
         raise ConversionError(
-            'Не удалось преобразовать ответ от API в JSON'
+            'Не удалось преобразовать ответ от API в JSON.'
         )
     else:
         logger.info(
-            'Ответ от API преобразован в JSON'
+            'Ответ от API преобразован в JSON.'
         )
-    logger.debug('Получен ответ от сервера')
+    logger.debug('Получен ответ от сервера.')
     return response
 
 
 def check_response(response):
     """Проверка ответа API на соответствие документации."""
     if not isinstance(response, dict):
-        raise TypeError('Ответ API не является словарем')
+        raise TypeError('Ответ API не является словарем.')
     homeworks = response.get('homeworks')
     if homeworks is None:
-        raise KeyError('Отсутствует ключ homeworks')
+        raise KeyError('Отсутствует ключ homeworks.')
     if not isinstance(homeworks, list):
-        raise TypeError('Значение ключа "homeworks" не является списком')
+        raise TypeError('Значение ключа "homeworks" не является списком.')
     if 'current_date' not in response.keys():
-        raise KeyError('В ответе API отстутсвуют нужные ключи')
+        raise KeyError('В ответе API отстутсвуют нужные ключи.')
     return homeworks
 
 
 def parse_status(homework):
     """Извлечение информации о д/з статус этой работы."""
     if 'homework_name' not in homework:
-        raise KeyError('Ответ сервера не содержит нужных ключей')
+        raise KeyError('Ответ сервера не содержит нужных ключей.')
     homework_status = homework.get('status')
     if homework_status not in HOMEWORK_VERDICTS:
-        raise StatusError('Не корректный статус работы')
+        raise StatusError('Не корректный статус работы.')
     homework_name = homework.get('homework_name')
     verdict = HOMEWORK_VERDICTS.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -149,17 +149,17 @@ def main():
             if homeworks:
                 message = parse_status(homeworks[0])
             else:
-                message = 'Обновлений по домашней работе нет'
+                message = 'Обновлений по домашней работе нет.'
                 logger.debug(message)
             if message != previous_message:
-                logger.info('Новое сообщение')
+                logger.info('Новое сообщение!')
                 send_message(bot, message)
                 previous_message = message
             else:
-                logger.info('Нового сообщения нет')
+                logger.info('Нового сообщения нет!')
             current_timestamp = response.get('current_date')
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = f'Сбой в работе программы: {error}!'
             logger.error(message)
             if message != previous_message:
                 send_message(bot, message)
